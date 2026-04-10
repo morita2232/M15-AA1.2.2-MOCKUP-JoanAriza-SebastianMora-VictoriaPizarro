c93fc9d6e7f3e8d472c234495643685d06a9a156
Morita <sebastian.mora@estudiant.enti.cat>
2026-04-10 19:36:51 +0200

    Morita cookeo el sistema de items

--- MiCompiMon/Assets/Script/Item.cs
+++ MiCompiMon/Assets/Script/Item.cs
@@ -1,16 +1,46 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+
+    public string itemName;
+    public float itemValue;
+
+    public Sprite sprite;
+
+    public Image image;
+
+    public TextMeshProUGUI nameDisplay;
+    public TextMeshProUGUI priceDisplay;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (itemName == null)
+        {
+            itemName = "No hay nombre";
+            nameDisplay.text = itemName;
+        }
+        else
+        {
+            nameDisplay.text = itemName;
+        }
+
+        if(itemValue == 0f)
+        {
+            itemValue = 404f;
+            priceDisplay.text = itemValue.ToString();
+        }
+        else
+        {
+            priceDisplay.text = itemValue.ToString();
+        }
+
+        image.sprite = sprite;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
